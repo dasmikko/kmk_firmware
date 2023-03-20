f3318c66bf48b79ae7974b83e038041ff04488d2
dasmikko <dasmikko@gmail.com>
2023-03-20 15:59:57 +0100

    implement OLED timeout

--- kmk/extensions/peg_oled_display.py
+++ kmk/extensions/peg_oled_display.py
@@ -1,10 +1,12 @@
 import busio
-import gc
+import gc	
+
 
 import adafruit_displayio_ssd1306
 import displayio
 import terminalio
 from adafruit_display_text import label
+from kmk.kmktime import ticks_diff, ticks_ms
 
 from kmk.extensions import Extension
 
@@ -17,6 +19,7 @@ class Oled(Extension):
         group,
         oWidth=128,
         oHeight=64,
+        timeout=2000
         flip: bool = False,
     ):
         displayio.release_displays()
@@ -24,7 +27,9 @@ class Oled(Extension):
         self.rotation = 180 if flip else 0
         self._width = oWidth
         self._height = oHeight
-        self._prevLayers = 0
+        self._timeout = timeout
+        self._checkpoint = ticks_ms()
+        self._displayIsAwake = True
         gc.collect()
 
     
@@ -53,17 +58,34 @@ class Oled(Extension):
 
     def before_matrix_scan(self, sandbox):
         self.updateOLED(sandbox)
+        
+        '''
+        Put the OLED to sleep after some time, to prevent premature burn-in
+        '''
+        if ticks_diff(ticks_ms(), self._checkpoint) > self._timeout and self._displayIsAwake == True:
+            self._checkpoint = ticks_ms()
+            self._display.sleep()
+            self._displayIsAwake = False
+
         gc.collect()
         return
 
     def after_matrix_scan(self, sandbox):
-
+        '''
+        Wake the display on keypress and update the checkpoint
+        '''
+        if sandbox.matrix_update is not None and sandbox.matrix_update.pressed == True:
+            self._checkpoint = ticks_ms()
+            if self._displayIsAwake == False:
+                self._display.wake()
+                self._displayIsAwake = True
+        gc.collect()
         return
 
     def before_hid_send(self, sandbox):
         return
 
-    def after_hid_send(self, sandbox):
+    def after_hid_send(self, sandbox):        
         return
 
     def on_powersave_enable(self, sandbox):
@@ -71,3 +93,4 @@ class Oled(Extension):
 
     def on_powersave_disable(self, sandbox):
         return
+
